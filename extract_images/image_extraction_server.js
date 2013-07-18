b6eb0f14ba8e0fe3d0bd39c5c910da01b3105516
Rhema Linder <rhemalinder@gmail.com>
2013-07-17 23:32:54 -0500

    Added whole collection of urls for images and now include width in height in serialized json.

--- extract_images/image_extraction_server.js
+++ extract_images/image_extraction_server.js
@@ -16,13 +16,12 @@ if (system.args.length !== 2) {
         response.statusCode = 200;
         response.headers = {
             'Cache': 'no-cache',
-            'Content-Type': 'text/html'
+            'Content-Type': 'application/json'
         };
         
         var url = request.url.split("=")[1];
+        url = decodeURIComponent(url);
 
-
-		
 		//setTimeout( function(){
 		
 		var page = require('webpage').create();
@@ -38,10 +37,27 @@ if (system.args.length !== 2) {
 		    if ( status === "success" ) {
 		        page.includeJs("http://ajax.googleapis.com/ajax/libs/jquery/1.6.1/jquery.min.js", function() {
 		            page.evaluate(function() {
-		                //console.log("$(\"#intro\").text() -> " + $("#intro").text());
-		                console.log("URL:"+$($("img")[0]).attr("src"));
+		            	
+		            	
+		            	
+		            	var getImgSize = function(imgSrc) {
+						    var newImg = new Image();
+						    newImg.src = imgSrc;
+						    var height = newImg.height;
+						    var width = newImg.width;
+						    return {"width":width,"height": height, "image_src":imgSrc};
+						}
+		            	
+		                //console.log("URL:"+$($("img")[0]).attr("src"));
+		                var images = $("img");	
+		                var all=[];
+		                for(var i=0;i<images.length;i++)
+		                {
+		                	//console.log($(images[i]).attr("src"));
+		                	all.push( getImgSize( $(images[i]).attr("src") ));
+		                }
+		                console.log("URL:"+JSON.stringify(all));
 		            });
-		            //phantom.exit();
 		        });
 		    }
 		});
